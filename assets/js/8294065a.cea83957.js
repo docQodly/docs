d17654e44e705918088a8b084f429a46031fe421
basmaelyaagoubi <basmaelyaagoubi@users.noreply.github.com>
2024-05-09 20:21:00 +0000

    deploy: 47407f9e4da02cd6852b31adfd1faca84cca0a61

--- assets/js/8294065a.cea83957.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkqodly=self.webpackChunkqodly||[]).push([[4759],{37796:e=>{e.exports=JSON.parse('{"title":"API","description":"QodlyScript REST API","image":"/img/docusaurus.png","slug":"/category/API","permalink":"/docs/category/API","navigation":{"next":{"title":"HTTP REST API","permalink":"/docs/api/overview-api"}}}')}}]);
