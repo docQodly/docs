1f038c50f5b2a8eb5673f0e88f1d6747bd82fafd
Mohamed IDRISSI <med.idrissi@outlook.com>
2024-12-05 09:03:16 +0100

    docs(webform-editor): generate docs for v0.3.20

--- scripts/get-docs.js
+++ scripts/get-docs.js
@@ -8,12 +8,15 @@ mkdirSync(`${process.cwd()}/docs/customComponent/api-reference/${name}`, {
     recursive: true
 })
 
+console.log(`Fetching:
+    https://registry.npmjs.com/${lib}/${version}
+    https://unpkg.com/${lib}@${version}/docs/api.mdx`);
+
 Promise.all([
     fetch(`https://registry.npmjs.com/${lib}/${version}`),
     fetch(`https://unpkg.com/${lib}@${version}/docs/api.mdx`)
 ]).then(([first, second]) => {
     return Promise.all([first.json(), second.text()])
 }).then(([{ version }, docs]) => {
-
     writeFileSync(`${process.cwd()}/docs/customComponent/api-reference/${name}/v${version}.mdx`, docs, { encoding: 'utf-8' })
 })
